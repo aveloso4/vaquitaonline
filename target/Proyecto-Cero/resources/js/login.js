d1f04a8a1bc387bec09b6f4c13f60250254c203d
EugeValeiras <eugenio.valeiras@gmail.com>
2015-02-02 02:06:41 -0300

    primer template Signin

---
+++ target/Proyecto-Cero/resources/js/login.js
@@ -0,0 +1,13 @@
+//INITIALIZE GOOGLE MAP
+function initialize() {
+
+	//AUTOCOMPLETE FIELDS
+	alert("ENTRE");
+	var db = document.getElementById('desde-buscar');
+	var hb = document.getElementById('hasta-buscar');
+	var autocomplete = new google.maps.places.Autocomplete(db);
+	var autocomplete = new google.maps.places.Autocomplete(hb);
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
